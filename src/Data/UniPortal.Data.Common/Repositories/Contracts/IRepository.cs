7427a5e7dc0fe871cbcfcb4c6403e68b61785a9f
Danail <danail.iordanov@yahoo.com>
2019-08-24 13:33:19 +0300

    Implemented repository patern on top of Entity Framework Core

---
+++ src/Data/UniPortal.Data.Common/Repositories/Contracts/IRepository.cs
@@ -0,0 +1,23 @@
+﻿namespace UniPortal.Data.Common.Repositories.Contracts
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using UniPortal.Data.Common.Models;
+
+    public interface IRepository<TEntity> : IDisposable
+        where TEntity : UniPortalEntity
+    {
+        IQueryable<TEntity> GetAll();
+
+        TEntity GetById(string id);
+
+        Task AddAsync(TEntity entity);
+
+        void Update(TEntity entity);
+
+        void Delete(TEntity entity);
+
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ src/Data/UniPortal.Data/Repositories/EfRepository.cs
@@ -0,0 +1,59 @@
+﻿namespace UniPortal.Data.Repositories
+{
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using UniPortal.Data.Common.Models;
+    using UniPortal.Data.Common.Repositories.Contracts;
+
+    public class EfRepository<TEntity> : IRepository<TEntity>
+        where TEntity : UniPortalEntity
+    {
+        private UniPortalDbContext dbContext;
+        private DbSet<TEntity> dbSet;
+
+        public EfRepository(UniPortalDbContext context)
+        {
+            this.DbContext = context;
+            this.DbSet = this.DbContext.Set<TEntity>();
+        }
+
+        protected UniPortalDbContext DbContext
+        {
+            get => this.dbContext;
+            private set => this.dbContext = value ?? throw new ArgumentNullException(nameof(this.DbContext));
+        }
+
+        protected DbSet<TEntity> DbSet
+        {
+            get => this.dbSet;
+            private set => this.dbSet = value;
+        }
+
+        public Task AddAsync(TEntity entity) => this.DbSet.AddAsync(entity);
+
+        public void Delete(TEntity entity) => this.DbSet.Remove(entity);
+
+        public void Dispose() => this.DbContext.Dispose();
+
+        public IQueryable<TEntity> GetAll() => this.DbSet;
+
+        public IQueryable<TEntity> GetAllAsNoTracking() => this.DbSet.AsNoTracking();
+
+        public TEntity GetById(string id) => this.DbSet.Find(id);
+
+        public Task<int> SaveChangesAsync() => this.DbContext.SaveChangesAsync();
+
+        public void Update(TEntity entity)
+        {
+            var entry = this.DbContext.Entry(entity);
+            if (entry.State == EntityState.Detached)
+            {
+                this.DbContext.Attach(entity);
+            }
+
+            entry.State = EntityState.Modified;
+        }
+    }
+}
