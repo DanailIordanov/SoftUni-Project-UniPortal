a3d69f3db15a1e4b070f5624a234353837c5b987
Danail <danail.iordanov@yahoo.com>
2019-08-28 13:11:02 +0300

    Wrote the views for the Course entity with their models

---
+++ src/Web/UniPortal.Web.BindingModels/Courses/JoinBindingModel.cs
@@ -0,0 +1,16 @@
+﻿namespace UniPortal.Web.BindingModels.Courses
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using UniPortal.Data.Models;
+    using UniPortal.Services.Mapping.Contracts;
+
+    public class JoinBindingModel : IMapFrom<Course>, IMapTo<Course>
+    {
+        [Required]
+        public string Id { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
---
+++ src/Web/UniPortal.Web.ViewModels/Courses/DetailsViewModel.cs
@@ -0,0 +1,31 @@
+﻿namespace UniPortal.Web.ViewModels.Courses
+{
+    using System;
+    using UniPortal.Data.Models;
+    using UniPortal.Services.Mapping.Contracts;
+
+    public class DetailsViewModel : IMapFrom<Course>, IMapTo<Course>
+    {
+        public string Name { get; set; }
+
+        public string Aims { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public string Location { get; set; }
+
+        public string Room { get; set; }
+
+        public string Language { get; set; }
+
+        public string ExamType { get; set; }
+
+        public string Requirements { get; set; }
+
+        public string Certificate { get; set; }
+    }
+}
